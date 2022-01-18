5f20e0197f73c44902e79e9192508515b382fae7
pg1411 <90827832+pg1411@users.noreply.github.com>
2022-01-18 15:13:19 +0530

    commited

--- hello.java
+++ hello.java
@@ -1,8 +1,6 @@
 public class hello{
     public static void main(String []args){
-       String name;
-        String email;
-       
+       // add mutliarg constructor
         System.out.println("Hello, world!");
         //add constructor
         //add args constructor if needed
