53f1c53c5a41f4fc7aac276dbd2edabc4fbda761
pg1411 <90827832+pg1411@users.noreply.github.com>
2022-01-18 15:15:02 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
     public static void main(String []args){
-       // add mutliarg constructor
+       
         System.out.println("Hello, world!");
         //add constructor
         //add args constructor if needed
