586e664f920a3ef985c90a3bd86b645513b903f5
pg1411 <90827832+pg1411@users.noreply.github.com>
2022-01-18 15:12:18 +0530

    edited

--- hello.java
+++ hello.java
@@ -1,17 +1,13 @@
 public class hello{
     public static void main(String []args){
-       private String name;
-        private String email;
-        public hello(String name,String email){
-            this.name = name;
-            this.email=email;
-        }
-        public hello(){
-            //default constructor
-        }
+       String name;
+        String email;
+       
         System.out.println("Hello, world!");
         //add constructor
         //add args constructor if needed
         
     }
+
+    
 }
