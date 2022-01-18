aff05301926760443ae55d53b1dace74af43f574
pg1411 <90827832+pg1411@users.noreply.github.com>
2022-01-18 15:19:16 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
     public static void main(String []args){
-       
+      //this is comment 
         System.out.println("Hello, world!");
         //add constructor
         //add args constructor if needed
